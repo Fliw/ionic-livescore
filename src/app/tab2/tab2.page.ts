564d026bb7bdd4eafa7fa0d1f1b4bda230a50538
fliw <fliw@digitalkode.com>
2021-04-14 22:16:59 +0700

    CHORE(INIT) : Bismillah First Init

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {MATCH} from '../../assets/data';
 
 @Component({
+  
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  public match = MATCH;
   constructor() {}
 
 }
---
+++ src/assets/data.ts
@@ -0,0 +1,40 @@
+export const MATCH = {
+    events: [
+        {
+            idEvent: '602445',
+            strHomeTeam: 'Everton',
+            strAwayTeam: 'Leicester',
+            dateEvent: '2020-04-06',
+            dateEventLocal: '2020-04-06',
+            strDate: '06\/04\/20',
+            strTime: '19:00:00',
+        },
+        {
+            idEvent: '602446',
+            strHomeTeam: 'Man City',
+            strAwayTeam: 'Liverpool',
+            dateEvent: '2020-04-05',
+            dateEventLocal: null,
+            strDate: '05\/04\/20',
+            strTime: '15:30:00',
+        },
+        {
+            idEvent: '602442',
+            strHomeTeam: 'West Ham',
+            strAwayTeam: 'Chelsea',
+            dateEvent: '2020-04-05',
+            dateEventLocal: null,
+            strDate: '05\/04\/20',
+            strTime: '13:00:00',
+        },
+        {
+            idEvent: '602448',
+            strHomeTeam: 'Sheffield United',
+            strAwayTeam: 'Tottenham',
+            dateEvent: '2020-04-04',
+            dateEventLocal: null,
+            strDate: '04\/04\/20',
+            strTime: '16:30:00',
+            }
+    ]
+};
